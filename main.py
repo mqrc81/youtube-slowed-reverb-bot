b63f87183359f970ea7511128c95698514dbb3f6
Marc Schmidt <mrc.schmidt@gmx.net>
2024-02-08 19:37:32 +0100

    fetched daily viral spotify tracks

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def hello_world(s):
-    print('Hello World')
+import json
 
+import spotipy
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyClientCredentials
+
+SPOTIFY_USA_VIRAl_50_PLAYLIST_ID = '37i9dQZEVXbKuaTI1Z1Afx'
 
 if __name__ == '__main__':
-    hello_world('print')
+    load_dotenv()
+
+    sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials())
+    playlist = sp.playlist(SPOTIFY_USA_VIRAl_50_PLAYLIST_ID,
+                           market='US', fields='tracks.items.track(id,name,artists(id,name))')
+    print(json.dumps(playlist))
